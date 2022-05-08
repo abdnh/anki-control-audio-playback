ae4e33e8f081adb6743d103be10b8a4dab5d7230
Abdo <abd.nh25@gmail.com>
2022-05-08 05:13:42 +0300

    Refactor and add interface to play custom sounds
    I plan to use this in copyaround and other add-ons where I currently
    implement custom play buttons and pycmd handlers that don't play nicely
    with standard Anki's audio playback.

--- src/__init__.py
+++ src/__init__.py
@@ -1,29 +1,20 @@
-from dataclasses import dataclass
-import json
-from typing import Dict, List, Optional, Tuple, Any
+from typing import Any, List, Tuple
 
-import aqt
-import aqt.sound
-from aqt.sound import AVTag, av_player
-
-from aqt.qt import *
 from aqt import mw
 from aqt.gui_hooks import (
     reviewer_will_show_context_menu,
     state_shortcuts_will_change,
     webview_will_set_content,
-    av_player_did_begin_playing,
-    av_player_did_end_playing,
-    reviewer_will_play_answer_sounds,
-    reviewer_will_play_question_sounds,
 )
-from aqt.utils import tooltip
-from aqt.webview import WebContent
+from aqt.qt import *
 from aqt.reviewer import Reviewer
-from anki.cards import Card
+from aqt.webview import WebContent
 
+from .playback_controller import PlaybackController
 
 config = mw.addonManager.getConfig(__name__)
+playback_controller = PlaybackController(config)
+mw.playback_controller = playback_controller
 mw.addonManager.setWebExports(__name__, r"web/.*\.js")
 base_path = f"/_addons/{mw.addonManager.addonFromModule(__name__)}/web"
 
@@ -33,76 +24,20 @@ def append_webcontent(webcontent: WebContent, context: Any) -> None:
         webcontent.js.append(f"{base_path}/audio.js")
 
 
-def get_speed() -> float:
-    return aqt.sound.mpvManager.command("get_property", "speed")
-
-
-def get_speed_factor() -> float:
-    return float(config.get("speed_factor", 10)) / 100
-
-
-def add_speed(speed: float) -> None:
-    aqt.sound.mpvManager.command("add", "speed", speed)
-    tooltip(f"Audio Speed {speed:+}<br>Current Speed: {get_speed()}")
-
-
-def set_speed(speed: float) -> None:
-    aqt.sound.mpvManager.command("set_property", "speed", speed)
-    tooltip(f"Reset Speed: {get_speed()}")
-
-
-def reset_speed() -> None:
-    set_speed(1.0)
-    if mw.reviewer:
-        mw.reviewer.web.eval("resetAudioSpeeed();")
-
-
-def speed_up() -> None:
-    factor = get_speed_factor()
-    add_speed(factor)
-    if mw.reviewer:
-        mw.reviewer.web.eval(f"addAudioPlaybackRate({factor});")
-
-
-def slow_down() -> None:
-    factor = -get_speed_factor()
-    add_speed(factor)
-    if mw.reviewer:
-        mw.reviewer.web.eval(f"addAudioPlaybackRate({factor});")
-
-
-# FIXME: queue can be get 'stuck' in only one side even when the option to include both sides's sounds in the back is enabled
-# we probably need to monkey-patch Reviewer.replayAudio to work around this.
-
-
-def play_n(i: int) -> None:
-    if av_player._enqueued:
-        av_player.clear_queue_and_maybe_interrupt()
-        # if there are any queued sounds, interrupt the current sound and continue playing the next ones
-        next_tags = sound_tags[current_sound.side][current_sound.index + i :]
-        av_player.play_tags(next_tags)
-    else:
-        av_player._stop_if_playing()
-        side = current_sound.side
-        tags = sound_tags[side]
-        idx = (current_sound.index + i) % len(tags)
-        av_player.play_tags([tags[idx]])
-
-
-def play_next() -> None:
-    play_n(1)
-
-
-def play_previous() -> None:
-    play_n(-1)
-
-
 actions = [
-    ("Speed Up Audio", config["speed_up_shortcut"], speed_up),
-    ("Slow Down Audio", config["slow_down_shortcut"], slow_down),
-    ("Reset Audio Speed", config["reset_speed_shortcut"], reset_speed),
-    ("Play Next Audio", config["play_next_shortcut"], play_next),
-    ("Play Previous Audio", config["play_previous_shortcut"], play_previous),
+    ("Speed Up Audio", config["speed_up_shortcut"], playback_controller.speed_up),
+    ("Slow Down Audio", config["slow_down_shortcut"], playback_controller.slow_down),
+    (
+        "Reset Audio Speed",
+        config["reset_speed_shortcut"],
+        playback_controller.reset_speed,
+    ),
+    ("Play Next Audio", config["play_next_shortcut"], playback_controller.play_next),
+    (
+        "Play Previous Audio",
+        config["play_previous_shortcut"],
+        playback_controller.play_previous,
+    ),
 ]
 
 
@@ -119,69 +54,7 @@ def add_menu_items(reviewer: Reviewer, menu: QMenu) -> None:
         qconnect(action.triggered, cb)
 
 
-sound_tags: Dict[str, List[AVTag]] = {
-    "a": [],
-    "q": [],
-}
-
-
-@dataclass
-class SoundTagInfo:
-    side: str
-    index: int
-    tag: AVTag
-
-
-current_sound: Optional[SoundTagInfo]
-
-
-def save_question_sound_tags(card: Card, tags: List[AVTag]) -> None:
-    global sound_tags
-    sound_tags["q"] = tags
-
-
-def save_answer_sound_tags(card: Card, tags: List[AVTag]) -> None:
-    global sound_tags
-    sound_tags["a"] = tags
-
-
-def on_begin_playing(player: aqt.sound.Player, tag: AVTag) -> None:
-    idx = -1
-    side = ""
-    for key, tags in sound_tags.items():
-        try:
-            idx = tags.index(tag)
-            side = key
-            break
-        except ValueError:
-            pass
-    if not side:
-        return
-
-    mw.reviewer.web.eval(
-        "setTimeout(() => setPlayButtonHighlight({}, {}, {}), 100);".format(
-            json.dumps(side),
-            json.dumps(idx),
-            json.dumps(config["play_button_highlight_color"]),
-        )
-    )
-
-    global current_sound
-    current_sound = SoundTagInfo(side, idx, tag)
-
-
-def clear_sound_tag_highlight(player: aqt.sound.Player) -> None:
-    mw.reviewer.web.eval(
-        """
-        setTimeout(clearPlayButtonsHighlight, 100);
-        """
-    )
-
-
-reviewer_will_show_context_menu.append(add_menu_items)
+playback_controller.init_hooks()
 state_shortcuts_will_change.append(add_state_shortcuts)
 webview_will_set_content.append(append_webcontent)
-av_player_did_begin_playing.append(on_begin_playing)
-av_player_did_end_playing.append(clear_sound_tag_highlight)
-reviewer_will_play_question_sounds.append(save_question_sound_tags)
-reviewer_will_play_answer_sounds.append(save_answer_sound_tags)
+reviewer_will_show_context_menu.append(add_menu_items)
---
+++ src/playback_controller.py
@@ -0,0 +1,242 @@
+import json
+import re
+from dataclasses import dataclass
+from typing import Any, Dict, List, Match, Optional, Tuple
+
+import aqt
+import aqt.sound
+from anki.cards import Card
+from anki.sound import AVTag, SoundOrVideoTag
+from aqt import mw
+from aqt.gui_hooks import (
+    av_player_did_begin_playing,
+    av_player_did_end_playing,
+    reviewer_will_play_answer_sounds,
+    reviewer_will_play_question_sounds,
+    webview_did_receive_js_message,
+)
+from aqt.qt import *
+from aqt.sound import AVTag, av_player
+from aqt.utils import tooltip
+
+
+@dataclass
+class SoundTagInfo:
+    side: str
+    index: int
+    tag: AVTag
+
+
+FILTER_NAME = "playback_controller"
+SOUND_REF_RE = re.compile(r"\[sound:(.*?)\]")
+PLAY_BUTTON = """<a class="replay-button soundLink" href=# onclick="pycmd('{cmd}:{side}:{idx}'); return false;">
+    <svg class="playImage" viewBox="0 0 64 64" version="1.1">
+        <circle cx="32" cy="32" r="29" />
+        <path d="M56.502,32.301l-37.502,20.101l0.329,-40.804l37.173,20.703Z" />
+    </svg>
+</a>"""
+
+
+class SoundTagList:
+    def __init__(self) -> None:
+        self.qtags: List[AVTag] = []
+        self.atags: List[AVTag] = []
+
+    def get_side(self, side: str) -> List[AVTag]:
+        if side == "q":
+            return self.qtags
+        else:
+            return self.atags
+
+
+class PlaybackController:
+    def __init__(self, config: Dict):
+        self.config = config
+        self.sound_tags = SoundTagList()
+        self.extra_tags = SoundTagList()
+        self.current_sound: Optional[SoundTagInfo] = SoundTagInfo(
+            side="q", index=0, tag=SoundOrVideoTag(filename="")
+        )
+
+    def get_speed(self) -> float:
+        return aqt.sound.mpvManager.command("get_property", "speed")
+
+    def get_speed_factor(self) -> float:
+        return float(self.config.get("speed_factor", 10)) / 100
+
+    def add_speed(self, speed: float) -> None:
+        aqt.sound.mpvManager.command("add", "speed", speed)
+        tooltip(f"Audio Speed {speed:+}<br>Current Speed: {self.get_speed()}")
+
+    def set_speed(self, speed: float) -> None:
+        aqt.sound.mpvManager.command("set_property", "speed", speed)
+        tooltip(f"Reset Speed: {self.get_speed()}")
+
+    def reset_speed(self) -> None:
+        self.set_speed(1.0)
+        mw.reviewer.web.eval("resetAudioSpeeed();")
+
+    def speed_up(self) -> None:
+        factor = self.get_speed_factor()
+        self.add_speed(factor)
+        mw.reviewer.web.eval(f"addAudioPlaybackRate({factor});")
+
+    def slow_down(self) -> None:
+        factor = -self.get_speed_factor()
+        self.add_speed(factor)
+        if mw.reviewer:
+            mw.reviewer.web.eval(f"addAudioPlaybackRate({factor});")
+
+    def save_question_sound_tags(self, card: Card, tags: List[AVTag]) -> None:
+        self.sound_tags.qtags = tags.copy()
+        self.extra_tags.qtags = []
+
+    def save_answer_sound_tags(self, card: Card, tags: List[AVTag]) -> None:
+        self.sound_tags.atags = tags.copy()
+        self.extra_tags.atags = []
+
+    def on_began_playing(self, player: aqt.sound.Player, tag: AVTag) -> None:
+        idx = -1
+        side = ""
+        tag_dict = {
+            "a": self.sound_tags.atags + self.extra_tags.atags,
+            "q": self.sound_tags.qtags + self.extra_tags.qtags,
+        }
+        for key, tags in tag_dict.items():
+            try:
+                idx = tags.index(tag)
+                side = key
+                break
+            except ValueError:
+                pass
+        if not side:
+            return
+
+        mw.reviewer.web.eval(
+            "setTimeout(() => setPlayButtonHighlight({}, {}, {}), 100);".format(
+                json.dumps(side),
+                json.dumps(idx),
+                json.dumps(self.config["play_button_highlight_color"]),
+            )
+        )
+        self.current_sound = SoundTagInfo(side, idx, tag)
+
+    def clear_sound_tag_highlight(self, player: aqt.sound.Player) -> None:
+        mw.reviewer.web.eval(
+            """
+            setTimeout(clearPlayButtonsHighlight, 100);
+            """
+        )
+
+    # FIXME: queue can be get 'stuck' in only one side even when the option to include both sides's sounds in the back is enabled
+    # we probably need to monkey-patch Reviewer.replayAudio to work around this.
+
+    def play_n(self, i: int) -> None:
+        if av_player._enqueued:
+            av_player.clear_queue_and_maybe_interrupt()
+            # if there are any queued sounds, interrupt the current sound and continue playing the next ones
+            side = self.current_sound.side
+            tags = self.sound_tags.get_side(side) + self.extra_tags.get_side(side)
+            next_tags = tags[self.current_sound.index + i :]
+            av_player.play_tags(next_tags)
+        else:
+            av_player._stop_if_playing()
+            side = self.current_sound.side
+            tags = self.sound_tags.get_side(side) + self.extra_tags.get_side(side)
+            idx = (self.current_sound.index + i) % len(tags)
+            av_player.play_tags([tags[idx]])
+
+    def play_next(self) -> None:
+        self.play_n(1)
+
+    def play_previous(self) -> None:
+        self.play_n(-1)
+
+    # def replay_audios(self) -> None:
+    #     reviewer = mw.reviewer
+    #     card = reviewer.card
+    #     if mw.reviewer.state == "question":
+    #         tags = card.question_av_tags()
+    #         self.sound_tags.qtags = tags
+    #         tags += self.extra_tags.qtags
+    #     else:
+    #         extra_tags = []
+    #         def_tags = []
+    #         if card.replay_question_audio_on_answer_side():
+    #             def_tags += card.question_av_tags()
+    #             extra_tags += self.extra_tags.qtags
+    #         def_tags += self.extra_tags.atags
+    #         extra_tags += self.extra_tags.atags
+    #         self.sound_tags.atags = def_tags
+    #         tags = extra_tags + def_tags
+    #     print(f"replay_audios: {tags=}")
+    #     av_player.play_tags(tags)
+    #     # reviewer.replayAudio()
+
+    # def modify_replay(self, state: str, shortcuts: List[Tuple[str, Callable]]) -> None:
+    #     if state == "review":
+    #         # modify the 'r' shortcut to give us replayed audios
+    #         for i, shortcut in enumerate(shortcuts):
+    #             if shortcut[0] == "r":
+    #                 shortcuts[i] = (shortcut[0], lambda: self.replay_audios())
+
+    def handle_js_msg(
+        self, handled: Tuple[bool, Any], message: str, context: Any
+    ) -> Tuple[bool, Any]:
+        if not message.startswith(FILTER_NAME):
+            return handled
+        _, subcmd, data = message.split(":", maxsplit=2)
+        if subcmd == "play":
+            filename = data
+            av_player.play_file(filename)
+        return (True, None)
+
+    def add_sound_tags_from_text(
+        self, text: str, side: str, play: bool = False
+    ) -> Tuple[str, List[AVTag]]:
+        tags = self.sound_tags.get_side(side) + self.extra_tags.get_side(side)
+        added: List[AVTag] = []
+
+        def repl_sounds(match: Match) -> str:
+            filename = match.group(1)
+            tag = SoundOrVideoTag(filename=filename)
+            added.append(tag)
+            return PLAY_BUTTON.format(
+                cmd="play", idx=len(tags) + len(added) - 1, side=side
+            )
+
+        text = SOUND_REF_RE.sub(repl_sounds, text)
+        added.reverse()
+        self.extra_tags.get_side(side).extend(added)
+        if play:
+            for tag in added:
+                av_player.insert_file(tag.filename)
+        return text, added
+
+    def apply_to_card_avtags(self, card: Card):
+        t = card.question_av_tags()
+        t.clear()
+        t.extend(self.sound_tags.qtags + self.extra_tags.qtags)
+        t = card.answer_av_tags()
+        t.clear()
+        t.extend(self.sound_tags.atags + self.extra_tags.atags)
+
+    def init_hooks(self) -> None:
+        av_player_did_begin_playing.append(
+            lambda player, tag: self.on_began_playing(player, tag)
+        )
+        av_player_did_end_playing.append(
+            lambda player: self.clear_sound_tag_highlight(player)
+        )
+        reviewer_will_play_question_sounds.append(
+            lambda card, tags: self.save_question_sound_tags(card, tags)
+        )
+        reviewer_will_play_answer_sounds.append(
+            lambda card, tags: self.save_answer_sound_tags(card, tags)
+        )
+        # state_shortcuts_will_change.append(
+        #     lambda state, shortcuts: self.modify_replay(state, shortcuts)
+        # )
+        webview_did_receive_js_message.append(
+            lambda handled, msg, context: self.handle_js_msg(handled, msg, context)
+        )
